583b099a0bed5a0f67fcc288bfbea9cf47ea6cb2
Gunnar Már Óttarsson <gunni@starcraft.is>
2020-12-04 17:17:54 +0000

    Update system.io.abstractions and move to sdk style proj

--- release-build-push.ps1
+++ release-build-push.ps1
@@ -1,4 +1,4 @@
-nuget restore
-nuget pack .\GMO.Versioning\ -build -Symbols -SymbolPackageFormat snupkg
+﻿dotnet restore
+dotnet pack .\GMO.Versioning\ -t:Build,Pack --include-symbols -p:Configuration=Release -o .
 $pkg = gci *.nupkg 
 nuget push $pkg -Source https://www.nuget.org/api/v2/package -NonInteractive
